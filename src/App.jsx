77b4987f5de168800e47bfdff702a37145fdef98
llynoirx <kathyho04@gmail.com>
2024-10-14 20:48:57 -0400

    callAPI ftn

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0)
+const API_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+const App = () => {
 
-export default App
+    const callAPI = async () => {
+      const query = "https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}"
+      const response = await fetch(query);
+      const json = await response.json();
+      try{
+        if (json.url == null) {
+          alert("Oops! Something went wrong with that query, let's try again!")
+        } else {
+          setCatImg(json.url);
+          setAttributes([]);
+        }
+      } catch (error) {
+        console.error('Failed to fetch data:', error);
+    }
+    }
+
+    return (
+        <div>
+           
+        </div>
+    );
+};
+
+export default App;
